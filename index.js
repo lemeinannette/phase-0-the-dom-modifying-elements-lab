0994c66afd507d315861bf62674dd287a3ff845f
Lemein <lemeinannette@gmail.com>
2023-10-07 15:24:39 +0300

    comp,eted lab

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-// Write your code here!
-const newHeader = document.createElement('h1');
-<script src="index.js"></script>
-
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+  
+    const searchKeyword = document.querySelector('#searchInput').value;
+  
+    fetch(`https://api.github.com/search/users?q=${searchKeyword}`, {
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the data and display user information
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  });
+  
